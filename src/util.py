4afa114df7cc01f594fd57e9cf37673aa3277ece
adakasky <robert910331@sina.com>
2018-05-02 18:33:45 -0400

    update util.py

--- src/util.py
+++ src/util.py
@@ -3,13 +3,15 @@ from __future__ import division
 from __future__ import print_function
 
 import pandas as pd
+import numpy as np
 
 
-def load_data(filename):
-    return pd.read_csv(filename, compression='gzip', header=0, sep=',', quotechar='"', error_bad_lines=False)
+def load_data(filename, columns=['rowc', 'colc', 'ra', 'field', 'fieldid', 'dec']):
+    return pd.read_csv(filename, compression='gzip', header=0, sep=',', quotechar='"', error_bad_lines=False)[columns]
 
 
 if __name__ == '__main__':
     data_path = "../data/sdss_100k.csv.gz"
-    data = load_data(data_path)
+    columns = ['rowc', 'colc', 'ra', 'field', 'fieldid', 'dec']
+    data = load_data(data_path, columns)
     print(data.shape)
