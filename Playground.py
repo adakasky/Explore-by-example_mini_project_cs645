24b585415f671096ce4028d7dcbd5ba68a31a7b9
adakasky <robert910331@sina.com>
2018-05-04 11:28:12 -0400

    update sample.py, need to edit sample method of false negatives

--- Playground.py
+++ Playground.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[1]:
@@ -12,81 +11,83 @@ from sklearn.cluster import KMeans
 
 normalize = lambda a: (a - a.min()) / (a.max() - a.min()) * 100
 
+
 class Sampler:
+    
+    def __init__(self, database, user, query, keys):
+        
+        self.keys = keys
+        self.conn = psycopg2.connect("dbname=" + database + " user=" + user)
+        self.cur = self.conn.cursor()
+        self.cur.execute(query)
+        self.conn.commit()
+        self.cache = np.array(self.cur.fetchall())
+    
+    def data_setup(self, false_query, size=3):
+        true_sample = self.cache[np.random.randint(len(self.cache), size=size)]
+        self.cur.execute(false_query)
+        self.false_result = np.array(self.cur.fetchall())
+        self.conn.commit()
+        false_sample = self.false_result[np.random.randint(len(self.false_result), size=size)]
+        return self.cache, true_sample, false_sample
+    
+    '''
+    @clf: a decision tree classifier
+    @training_data: dataset to be sampled
+    @k: number of iteration
+    @y: distance
+    '''
+    
+    # sample(tree, false_negative, clusters, y)
+    def sample(self, clf, training_data, k=6, y=1, f=10):
+        
+        prediction = clf.predict(training_data)
+        kmean_training = training_data[np.where(prediction == 0)]
+        if len(kmean_training) == 0:
+            return 'False'
+        kmean = KMeans(n_clusters=k, random_state=0).fit(kmean_training)
+        
+        # Map each tranining point to corresponding cluster
+        self.clusters = {l: [] for l in kmean.labels_}
+        # In each cluster, find out boundaries of each dimension
+        self.sample_info = {label: {dim: {'max': 0, 'min': 0} for dim in range(k)} for label in kmean.labels_}
+        
+        for l in range(len(kmean.labels_)):
+            self.clusters[kmean.labels_[l]].append(kmean_training[l])
+        
+        for l in self.sample_info.keys():
+            for i in range(k):
+                self.sample_info[kmean.labels_[l]][i]['max'] = np.max(self.clusters[kmean.labels_[l]], axis=0)[i]
+                self.sample_info[kmean.labels_[l]][i]['min'] = np.min(self.clusters[kmean.labels_[l]], axis=0)[i]
+        
+        Q = []
+        # For each attributes ...
+        for k in self.sample_info.keys():
+            # For each dimension in the keys ...
+            query = 'where '
+            for a in range(len(self.sample_info[k])):
+                query += keys[a] + ' between ' + str(self.sample_info[k][a]['min']) + ' and ' + str(
+                    self.sample_info[k][a]['max']) + ' and '
+            Q.append(query[:-4] + ' limit ' + str(f))
+        return Q
 
-	def __init__(self, database, user, query, keys):
-
-		self.keys = keys
-		self.conn = psycopg2.connect("dbname=" + database +  " user="+user)
-		self.cur = self.conn.cursor()
-		self.cur.execute(query)
-		self.conn.commit()
-		self.cache = np.array(self.cur.fetchall())
-
-	def data_setup(self, false_query, size=3):
-		true_sample = self.cache[np.random.randint(len(self.cache), size=size)]
-		self.cur.execute(false_query)
-		self.false_result = np.array(self.cur.fetchall())
-		self.conn.commit()
-		false_sample = self.false_result[np.random.randint(len(self.false_result), size=size)]
-		return self.cache, true_sample, false_sample
-
-	'''
-	@clf: a decision tree classifier 
-	@training_data: dataset to be sampled
-	@k: number of iteration
-	@y: distance 
-	'''
-	# sample(tree, false_negative, clusters, y)
-	def sample(self, clf, training_data, k=6, y=1, f=10):
-
-		prediction = clf.predict(training_data)
-		kmean_training = training_data[np.where(prediction == 0)]
-		if len(kmean_training) == 0:
-			return 'False'
-		kmean = KMeans(n_clusters=k, random_state=0).fit(kmean_training)
-		
-		# Map each tranining point to corresponding cluster
-		self.clusters = {l:[] for l in kmean.labels_}
-		# In each cluster, find out boundaries of each dimension
-		self.sample_info = {label:{dim:{'max':0, 'min':0} for dim in range(k)} for label in kmean.labels_}
-		
-		for l in range(len(kmean.labels_)):
-			self.clusters[kmean.labels_[l]].append(kmean_training[l])
-		
-		for l in self.sample_info.keys():
-			for i in range(k):
-				self.sample_info[kmean.labels_[l]][i]['max'] = np.max(self.clusters[kmean.labels_[l]], axis=0)[i] 
-				self.sample_info[kmean.labels_[l]][i]['min'] = np.min(self.clusters[kmean.labels_[l]], axis=0)[i] 
-
-		Q = []
-		# For each attributes ...
-		for k in self.sample_info.keys():
-			# For each dimension in the keys ...
-			query = 'where '
-			for a in range(len(self.sample_info[k])):
-				query += keys[a] + ' between ' + str(self.sample_info[k][a]['min']) + ' and ' + str(self.sample_info[k][a]['max']) + ' and '
-			Q.append(query[:-4] + ' limit ' + str(f))
-		return Q
 
 true_query = "select * from photoobjall where rowc > 39 and rowc < 77 and colc > 25 and colc < 56"
 false_query = "select * from photoobjall where rowc < 39 and colc > 25"
-keys = {0:'rowc',1:'colc',2:'ra', 3:'field',4:'fieldid',5:'dec'}
+keys = {0: 'rowc', 1: 'colc', 2: 'ra', 3: 'field', 4: 'fieldid', 5: 'dec'}
 
 sampler = Sampler('zitao', 'zitao', true_query, keys)
 
-#data = pd.read_csv('sdss_100k.csv')
-#clean_data = data.loc[:,['rowc','colc','ra','field','fieldid','dec']]
+# data = pd.read_csv('sdss_100k.csv')
+# clean_data = data.loc[:,['rowc','colc','ra','field','fieldid','dec']]
 
 cache, true_sample, false_sample = sampler.data_setup(false_query, 3)
 
 training = np.vstack((true_sample, false_sample))
 labels = np.array([1, 1, 1, 0, 0, 0])
-clf = tree.DecisionTreeClassifier(random_state = 0)
+clf = tree.DecisionTreeClassifier(random_state=0)
 clf.fit(training, labels)
 training_data = cache[np.random.randint(len(cache), size=1000)]
 
 Q = sampler.sample(clf, training_data)
 print(Q)
-
-
---
+++ src/sample.py
@@ -0,0 +1,84 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pandas as pd
+import numpy as np
+
+np.random.seed(0)
+
+from sklearn.cluster import KMeans
+
+from src.util import load_data
+
+
+class ClusterTreeNode(object):
+    def __init__(self, data, rel=False, k=4):
+        self.k = k
+        self.data = data
+        self.rel = rel
+    
+    def sample(self):
+        self.cluster = KMeans(n_clusters=self.k, random_state=0)
+        self.cluster.fit(self.data)
+        pred = self.cluster.labels_
+        centroids = self.cluster.cluster_centers_
+        self.clustering = np.array([np.array(data)[np.where(pred == i)] for i in range(self.k)])
+        self.boundaries = [{'min': self.clustering[i].min(axis=0),
+                            'max': self.clustering[i].max(axis=0)} for i in range(self.k)]
+        self.samples = np.array([self.clustering[i][np.argmin(np.sqrt(np.sum(
+            (self.clustering[i] - centroids[i]) ** 2, axis=1)))] for i in range(self.k)])
+    
+    def split(self):
+        self.relevant = []
+        self.irrelevant = []
+        relevance = (self.samples[:, 0] > 39) & (self.samples[:, 0] < 77) \
+                    & (self.samples[:, 1] > 25) & (self.samples[:, 1] < 56)
+        self.relevant.extend([ClusterTreeNode(c, rel=True, k=4)
+                              for i, c in enumerate(self.clustering) if relevance[i]])
+        self.irrelevant.extend([ClusterTreeIrrNode(c, rel=False, k=4)
+                                for i, c in enumerate(self.clustering) if not relevance[i]])
+
+
+class ClusterTreeRelNode(ClusterTreeNode):
+    def __init__(self, data, rel=True, k=4):
+        super(ClusterTreeRelNode, self).__init__(data, rel, k)
+    
+    def sample(self):  # sample around boundaries and shrink the boundaries
+        boundary_idx = np.hstack([self.data.argmin(axis=0), self.data.argmax(axis=0)])
+        self.samples = self.data[boundary_idx]
+        relevance = (self.samples[:, 0] > 39) & (self.samples[:, 0] < 77) \
+                    & (self.samples[:, 1] > 25) & (self.samples[:, 1] < 56)
+        del_idx = boundary_idx[np.where(~relevance)]
+        self.data = np.delete(self.data, del_idx, axis=0)
+        self.boundaries = [{'min': self.data.min(axis=0), 'max': self.data.max(axis=0)}]
+
+
+class ClusterTreeIrrNode(ClusterTreeNode):
+    def __init__(self, data, rel=False, k=4):
+        super(ClusterTreeIrrNode, self).__init__(data, rel, k)
+    
+    def split(self):
+        relevance = (self.samples[:, 0] > 39) & (self.samples[:, 0] < 77) \
+                    & (self.samples[:, 1] > 25) & (self.samples[:, 1] < 56)
+        self.relevant.extend([ClusterTreeFNNode(c, rel=True, k=self.k)
+                              for i, c in enumerate(self.clustering) if relevance[i]])
+        self.irrelevant.extend([ClusterTreeIrrNode(c, rel=False, k=self.k)
+                                for i, c in enumerate(self.clustering) if not relevance[i]])
+
+
+class ClusterTreeFNNode(ClusterTreeNode):
+    def __init__(self, data, rel=True, k=4):
+        super(ClusterTreeFNNode, self).__init__(data, rel, k)
+    
+    def sample(self):  # sample around centroid and expand the boundaries
+        pass
+
+
+if __name__ == '__main__':
+    data_path = "../data/sdss_100k.csv.gz"
+    columns = ['rowc', 'colc', 'ra', 'field', 'fieldid', 'dec']
+    data = np.array(load_data(data_path, columns))
+    
+    ground_truth = data[(data[:, 0] > 39) & (data[:, 0] < 77) & (data[:, 1] > 25) & (data[:, 1] < 56)]
+    root = ClusterTreeNode(data, k=4)
